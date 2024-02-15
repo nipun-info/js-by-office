714d214444f61e1a05baac3f3b28888669aad25e
nipunShah15 <nipun.info206@gmail.com>
2024-02-15 21:13:03 +0600

    JavaScript module25 excercise of office

---
+++ Event/event.js
@@ -0,0 +1,20 @@
+/*
+Searching Google or Youtube???
+
+important documents:
+https://tinycode.medium.com/10-of-the-most-important-document-events-5fd1879a5a1f
+
+event bubble:
+https://www.freecodecamp.org/news/event-bubbling-in-javascript/
+
+event.stopPropagation():
+
+event.stopImmediatePropagation():
+
+HTMLCollection(5): 
+
+Event Delegate:
+
+
+*/ 
+
